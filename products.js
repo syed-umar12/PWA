f04e8f602dd40f7f4a7f69932e06f0846f1c3af9
syed-umar12 <mohdumerr0@gmail.com>
2024-11-26 16:48:22 +0500

    NEW DATA

---
+++ products.js
@@ -0,0 +1,50 @@
+let products = [
+    {
+        "id":1001,
+        "title":"cat Food, 25lb bag",
+        "description": "Dell, global company that designs, develops, and manufactures personal computers (PCs) and a variety of computer-related products",
+        "price": 219,
+        "image": "laptop1.webp",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id":1002,
+        "title":"cat Food, 25lb bag",
+        "description": "HP (Hewlett-Packard) is a multinational information technology (IT) company that sells hardware, software and related business services.",
+        "price": 235,
+        "image": "laptop2.jfif",
+        "availableInventory": 7,
+        "rating": 2
+    },
+    {
+        "id":1003,
+        "title":"cat Food, 25lb bag",
+        "description": "MacBook is a brand of Mac notebook computers developed and marketed by Apple that use Apple's macOS operating system since 2006",
+        "price": 250,
+        "image": "laptop3.jfif",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id":1004,
+        "title":"cat Food, 25lb bag",
+        "description": "a portable microcomputer having its main components (such as processor, keyboard, and display screen) integrated into a single unit capable of battery-powered operation.",
+        "price": 310,
+        "image": "laptop4.jfif",
+        "availableInventory": 6,
+        "rating": 2
+    },
+    {
+        "id":1005,
+        "title":"cat Food, 25lb bag",
+        "description": "The company is one of the world's leading computer brands and suppliers of PCs. Dell is headquartered in Round Rock,",
+        "price": 110,
+        "image": "laptop5.jfif",
+        "availableInventory": 9,
+        "rating": 2
+    }
+
+      
+   
+]
